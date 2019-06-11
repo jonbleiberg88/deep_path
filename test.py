e891f06c4120c6bbc57f5a39835b555c94eb41d0
Jonathan Bleiberg <jlbleiberg@gmail.com>
2019-06-11 18:23:05 -0400

    tweak

--- test.py
+++ test.py
@@ -3,4 +3,5 @@ from utils.preprocessing_utils import fix_filenames
 dir = "/data/jblei/cancer_project/datasets/FL/256_0_4_processed/data"
 folds = 5
 if __name__ == '__main__':
-    split_train_test(dir, 5)
+    # split_train_test(dir, 5)
+    fix_filenames(dir)
