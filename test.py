e74dab0cfdcda46ee9421398ebc0abdbcb56bc7b
Jonathan Bleiberg <jlbleiberg@gmail.com>
2019-06-11 17:27:00 -0400

    testing...

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from utils.train_utils import split_train_test
 from utils.preprocessing_utils import fix_filenames
-dir = "/Volumes/Elements/5:21:19/Datasets/FL/test/data/"
+dir = "/data/jblei/cancer_project/datasets/FL/256_0_4_processed/data"
 folds = 5
 if __name__ == '__main__':
-    fix_filenames(dir)
+    split_train_test(dir, 5)
