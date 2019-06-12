a47d2c1850fa3d2c4699268d259c832814de3fca
Jonathan Bleiberg <jlbleiberg@gmail.com>
2019-06-12 13:28:10 -0400

    pil fix

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from utils.train_utils import split_train_test
-from utils.preprocessing_utils import fix_filenames
+#from utils.preprocessing_utils import fix_filenames
 dir = "/data/jblei/cancer_project/datasets/FL/256_0_4_processed/data"
 folds = 5
 if __name__ == '__main__':
