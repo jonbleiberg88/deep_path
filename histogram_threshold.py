8cadb3217ad4fc61ef4c49a774baa35d9bd37c91
Jonathan Bleiberg <jlbleiberg@gmail.com>
2019-06-14 16:49:40 -0400

    Add default dir

--- histogram_threshold.py
+++ histogram_threshold.py
@@ -1,4 +1,5 @@
 from utils.preprocessing_utils import *
+import constants
 import argparse
 import os
 import sys
@@ -6,7 +7,7 @@ import sys
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--dir", "-d", help = "Directory where the dataset is stored", required=True)
+    parser.add_argument("--dir", "-d", help = "Directory where the dataset is stored", required=True, default=constants.PATCH_OUTPUT_DIRECTORY)
     parser.add_argument("--black_white_threshold", "-bw", type=float, required=False, default=0.9)
     parser.add_argument("--remove_threshold", "-t", type=float, required=False, default=0.9)
     parser.add_argument("--blur_radius", "-r", type=int, required=False, default=7)
