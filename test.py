a21340e58dccbdcc1fa73024d258c188442ce2ad
Jonathan Bleiberg <jlbleiberg@gmail.com>
2019-06-14 17:11:31 -0400

    filename fix

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-from utils.train_utils import split_train_test
-#from utils.preprocessing_utils import fix_filenames
+# from utils.train_utils import split_train_test
+from utils.preprocessing_utils import fix_filenames
+import constants
 dir = "/data/jblei/cancer_project/datasets/FL/256_0_4_processed/data"
 folds = 5
 if __name__ == '__main__':
-    split_train_test(dir, 5)
-    #fix_filenames(dir)
+    # split_train_test(dir, 5)
+    fix_filenames(constants.PATCH_OUTPUT_DIRECTORY)
