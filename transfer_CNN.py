f414d7924e2084a75dac4c3fac992893b49720e6
Jonathan Bleiberg <jlbleiberg@gmail.com>
2019-06-18 14:40:45 -0400

    tweaks

--- transfer_CNN.py
+++ transfer_CNN.py
@@ -4,7 +4,7 @@ from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras.layers import Dense, Flatten, BatchNormalization, Dropout
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import SGD
-form tenforflow.keras.utils import multi_gpu_model
+from tensorflow.keras.utils import multi_gpu_model
 
 class TransferCNN:
     def __init__(self, input_shape=(256,256,3), base_model=ResNet50,layer_sizes=[512],
