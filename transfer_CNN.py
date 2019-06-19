5dad291dc4678698a8db445dbf75598f5bccbcea
Jonathan Bleiberg <jlbleiberg@gmail.com>
2019-06-18 20:57:06 -0400

    tweaks

--- transfer_CNN.py
+++ transfer_CNN.py
@@ -23,7 +23,7 @@ class TransferCNN:
 
 
     def init_model(self):
-        layer_list = [self.base_model, Flatten()]
+        layer_list = [self.base_model, Flatten(), BatchNormalization()]
         for units in self.layer_sizes:
             layer_list.append(Dense(units, activation='relu'))
             if self.use_bn:
