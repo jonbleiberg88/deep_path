5d2d9c7322b895685276bffabe7226cd82864809
Jonathan Bleiberg <jlbleiberg@gmail.com>
2019-06-25 17:59:15 -0400

    tweaks

--- data_generator.py
+++ data_generator.py
@@ -294,12 +294,15 @@ class TestDataGenerator(tf.keras.utils.Sequence):
 
     def __len__(self):
         'Denotes the number of batches per epoch'
-        return int(np.floor(len(self.paths) / self.batch_size))
+        return int(np.ceil(len(self.paths) / self.batch_size))
 
     def __getitem__(self, index):
         'Generate one batch of data'
         # Generate indexes of the batch
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        if (index + 1) * self.batch_size > len(self.indexes):
+            indexes = self.indexes[index*self.batch_size:-1]
+        else:
+            indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
 
         # Find list of IDs
         batch_paths = self.paths[indexes]
@@ -318,8 +321,9 @@ class TestDataGenerator(tf.keras.utils.Sequence):
     def __data_generation(self, batch_paths, batch_labels):
         'Generates data containing batch_size samples' # X : (n_samples, *out_dim, n_channels)
         # Initialization
-        X = np.empty((self.batch_size, *self.out_dim, self.n_channels))
-        y = np.empty((self.batch_size), dtype=int)
+        size = len(batch_paths)
+        X = np.empty((size, *self.out_dim, self.n_channels))
+        y = np.empty((size), dtype=int)
 
         # Generate data
         for i, data in enumerate(zip(batch_paths, batch_labels)):
