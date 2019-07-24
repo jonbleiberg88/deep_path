1709fab3ebc723f927a91266932bdabe75415f03
Jonathan Bleiberg <jlbleiberg@gmail.com>
2019-07-24 19:27:48 -0400

    tuning

--- transfer_CNN.py
+++ transfer_CNN.py
@@ -16,7 +16,7 @@ import constants
 # SGD(lr=0.2, decay=1e-6, momentum=0.9,nesterov=True)
 
 class TransferCNN:
-    def __init__(self, input_shape=constants.INPUT_SHAPE, base_model=DenseNet121,layer_sizes=constants.LAYER_SIZES,
+    def __init__(self, input_shape=constants.INPUT_SHAPE, base_model=MobileNet,layer_sizes=constants.LAYER_SIZES,
         n_classes=2, use_bn=constants.USE_BATCH_NORM, use_dropout=constants.USE_DROPOUT,
         optimizer='adam', metrics=constants.METRICS):
         self.input_shape = input_shape
