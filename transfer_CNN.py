890fcbbcd58e5fdaf92552c5d109c399c4e9b3e2
Jonathan Bleiberg <jlbleiberg@gmail.com>
2019-07-24 19:31:24 -0400

    tuning

--- transfer_CNN.py
+++ transfer_CNN.py
@@ -70,8 +70,8 @@ class TransferCNN:
 
 
 def class_0_acc(y_true, y_pred):
-    class_id_true = K.argmax(y_true, axis=-1)
-    class_id_preds = K.argmax(y_pred, axis=-1)
+    class_id_true = K.round(y_true)
+    class_id_preds = K.round(y_pred)
     # Replace class_id_preds with class_id_true for recall here
     accuracy_mask = K.cast(K.equal(class_id_preds, 0), 'int32')
     class_acc_tensor = K.cast(K.equal(class_id_true, class_id_preds), 'int32') * accuracy_mask
@@ -79,8 +79,8 @@ def class_0_acc(y_true, y_pred):
     return class_acc
 
 def class_1_acc(y_true, y_pred):
-    class_id_true = K.argmax(y_true, axis=-1)
-    class_id_preds = K.argmax(y_pred, axis=-1)
+    class_id_true = K.round(y_true)
+    class_id_preds = K.round(y_pred)
     # Replace class_id_preds with class_id_true for recall here
     accuracy_mask = K.cast(K.equal(class_id_preds, 1), 'int32')
     class_acc_tensor = K.cast(K.equal(class_id_true, class_id_preds), 'int32') * accuracy_mask
