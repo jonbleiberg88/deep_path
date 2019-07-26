31148cc2c961af95bc53b8e5fb1a8f16dc4c2df1
Jonathan Bleiberg <jlbleiberg@gmail.com>
2019-07-26 14:14:09 -0400

    gpu mem allocation

--- transfer_CNN.py
+++ transfer_CNN.py
@@ -4,7 +4,7 @@ from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras.applications.xception import Xception
 from tensorflow.keras.applications.mobilenet import MobileNet, preprocess_input
-from tensorflow.keras.applications.densenet import DenseNet121, preprocess_input
+from tensorflow.keras.applications.densenet import DenseNet121
 # from tensorflow.keras.applications.vgg19 import VGG19, preprocess_input
 from tensorflow.keras.layers import Dense, Flatten, BatchNormalization, Dropout
 from tensorflow.keras.models import Sequential
@@ -16,10 +16,10 @@ import constants
 # SGD(lr=0.2, decay=1e-6, momentum=0.9,nesterov=True)
 
 class TransferCNN:
-    def __init__(self, input_shape=constants.INPUT_SHAPE, base_model=DenseNet121,layer_sizes=constants.LAYER_SIZES,
+    def __init__(self, input_shape=constants.INPUT_SHAPE, base_model=MobileNet,layer_sizes=constants.LAYER_SIZES,
         n_classes=2, use_bn=constants.USE_BATCH_NORM, use_dropout=constants.USE_DROPOUT,
         optimizer='adam', metrics=constants.METRICS):
-        
+
         if constants.CAP_MEMORY_USAGE:
             # https://michaelblogscode.wordpress.com/2017/10/10/reducing-and-profiling-gpu-memory-usage-in-keras-with-tensorflow-backend/
             # TensorFlow wizardry
