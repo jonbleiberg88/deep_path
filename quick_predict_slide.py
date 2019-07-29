bc6a9434ed0d2addb51503894fc3a795cedb93ca
Jonathan Bleiberg <jlbleiberg@gmail.com>
2019-07-29 19:45:11 -0400

    predict aggregation metrics

--- quick_predict_slide.py
+++ quick_predict_slide.py
@@ -55,12 +55,49 @@ def get_overall_metrics(label_to_class, predict_dir=constants.PREDICTIONS_DIRECT
 
     acc = np.mean(np.array(correct_list))
 
-    print(confusion_mat)
+    print_cm(confusion_mat, [label_to_class[0], label_to_class[1]])
 
     print("_________________________________________________________________")
     print(f"Patient Level Accuracy: {acc*100:.2f}% ({num_correct}/{num_slides})")
     print("_________________________________________________________________")
     return acc, num_correct, num_slides
+
+
+def print_cm(cm, labels, hide_zeroes=False, hide_diagonal=False, hide_threshold=None):
+    """
+    Pretty print for confusion matrices
+    from https://gist.github.com/zachguo/10296432
+    """
+    columnwidth = max([len(x) for x in labels] + [5])  # 5 is value length
+    empty_cell = " " * columnwidth
+
+    # Begin CHANGES
+    fst_empty_cell = (columnwidth-3)//2 * " " + "t/p" + (columnwidth-3)//2 * " "
+
+    if len(fst_empty_cell) < len(empty_cell):
+        fst_empty_cell = " " * (len(empty_cell) - len(fst_empty_cell)) + fst_empty_cell
+    # Print header
+    print("    " + fst_empty_cell, end=" ")
+    # End CHANGES
+
+    for label in labels:
+        print("%{0}s".format(columnwidth) % label, end=" ")
+
+    print()
+    # Print rows
+    for i, label1 in enumerate(labels):
+        print("    %{0}s".format(columnwidth) % label1, end=" ")
+        for j in range(len(labels)):
+            cell = "%{0}s".format(columnwidth) % cm[i, j]
+            if hide_zeroes:
+                cell = cell if float(cm[i, j]) != 0 else empty_cell
+            if hide_diagonal:
+                cell = cell if i != j else empty_cell
+            if hide_threshold:
+                cell = cell if cm[i, j] > hide_threshold else empty_cell
+            print(cell, end=" ")
+        print()
+
 
 if __name__ == "__main__":
     class_to_label = load_pickle_from_disk(os.path.join(constants.VISUALIZATION_HELPER_FILE_FOLDER, 'class_to_label'))
